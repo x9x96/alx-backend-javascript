ec611c711a8b52a3dc8d0fd3e574b9bec7823322
x9x96 <israelsbook@gmail.com>
2024-02-02 16:47:30 -0800

    patterning 4

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,6 +1,7 @@
 const Utils = require("./utils");
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
 }
 
 module.exports = sendPaymentRequestToApi;
